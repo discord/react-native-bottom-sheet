5c9e4386a7adf70f57a8fcd0bcc708be1420584d
Michael Fong (Meishu) <michael.fong@discordapp.com>
2023-01-19 08:03:39 -0800

    convert footer component to render function

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -157,7 +157,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
       handleComponent,
       backdropComponent,
       backgroundComponent,
-      footerComponent,
+      renderFooter,
       children: Content,
     } = props;
     //#endregion
@@ -1644,10 +1644,8 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
                   >
                     {typeof Content === 'function' ? <Content /> : Content}
 
-                    {footerComponent && (
-                      <BottomSheetFooterContainer
-                        footerComponent={footerComponent}
-                      />
+                    {renderFooter && (
+                      <BottomSheetFooterContainer renderFooter={renderFooter} />
                     )}
                   </BottomSheetDraggableView>
                 </Animated.View>
