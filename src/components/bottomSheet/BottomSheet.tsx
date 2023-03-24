e63e6b760b6a0683f1a618fc8b59aad4334f50d2
Marcus Huderle <huderlem@gmail.com>
2023-03-24 17:27:52 -0400

    Add support for an initial scroll buffer for Scrollable components (#7)

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -281,6 +281,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
       animatedScrollableContentOffsetY,
       animatedScrollableOverrideState,
       isScrollableRefreshable,
+      isScrollableLocked,
       setScrollableRef,
       removeScrollableRef,
     } = useScrollable();
@@ -370,6 +371,13 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
        * if sheet state is extended, then unlock scrolling
        */
       if (animatedSheetState.value === SHEET_STATE.EXTENDED) {
+        return SCROLLABLE_STATE.UNLOCKED;
+      }
+
+      /**
+       * if the current scrollable is blocked from translation, unlock scrolling
+       */
+      if (!isScrollableLocked.value) {
         return SCROLLABLE_STATE.UNLOCKED;
       }
 
@@ -1085,6 +1093,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
         isInTemporaryPosition,
         isContentHeightFixed,
         isScrollableRefreshable,
+        isScrollableLocked,
         shouldHandleKeyboardEvents,
         simultaneousHandlers: _providedSimultaneousHandlers,
         waitFor: _providedWaitFor,
@@ -1120,6 +1129,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
         shouldHandleKeyboardEvents,
         animatedScrollableContentOffsetY,
         isScrollableRefreshable,
+        isScrollableLocked,
         isContentHeightFixed,
         isInTemporaryPosition,
         enableContentPanningGesture,
