42aa4f9f67ef4ff0472c9da5133e7dff2a3d8996
Amadeus Demarzi <amadeus@users.noreply.github.com>
2023-08-07 12:42:49 -0700

    Reanimated update fixes (#9)
    * chore: update reanimated and typescript deps
    
    * chore: misc other type updates

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -34,6 +34,7 @@ import {
   BottomSheetInternalProvider,
   BottomSheetProvider,
 } from '../../contexts';
+import type { BottomSheetInternalContextType } from '../../contexts/internal';
 import BottomSheetContainer from '../bottomSheetContainer';
 import BottomSheetGestureHandlersProvider from '../bottomSheetGestureHandlersProvider';
 import BottomSheetBackdropContainer from '../bottomSheetBackdropContainer';
@@ -352,7 +353,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
       isInTemporaryPosition,
       keyboardBehavior,
     ]);
-    const animatedScrollableState = useDerivedValue(() => {
+    const animatedScrollableState = useDerivedValue<SCROLLABLE_STATE>(() => {
       /**
        * if scrollable override state is set, then we just return its value.
        */
@@ -1065,7 +1066,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
     //#endregion
 
     //#region contexts variables
-    const internalContextVariables = useMemo(
+    const internalContextVariables = useMemo<BottomSheetInternalContextType>(
       () => ({
         enableContentPanningGesture,
         overDragResistanceFactor,
