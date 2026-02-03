97f6d20df049a0b6b9005c237001852a353a2931
jixwanwang <jixuan.wang@gmail.com>
2026-02-03 15:16:10 -0800

    Update createBottomSheetScrollableComponent.tsx

--- src/components/bottomSheetScrollable/createBottomSheetScrollableComponent.tsx
+++ src/components/bottomSheetScrollable/createBottomSheetScrollableComponent.tsx
@@ -85,7 +85,7 @@ export function createBottomSheetScrollableComponent<T, P>(
       [animatedScrollableState, showsVerticalScrollIndicator, preserveScrollMomentum]
     );
 
-    React.useEffect(() => {console.log('testing code');}
+    React.useEffect(() => {console.log('testing code');});
     
     const scrollableGesture = useMemo(
       () =>
